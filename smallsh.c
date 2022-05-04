4b41de2c760038a5aa236ab2e47d4066ea210d3d
Tyler Wennstrom <tylerwennstrom@gmail.com>
2022-05-04 13:57:19 -0700

    logic for checking args

--- smallsh.c
+++ smallsh.c
@@ -25,6 +25,33 @@ int main(int argc, char *argv[])
         // use the colon symbol as a prompt for each command line
         printf(": ");
 
+        // get input from command line
+        FILE *input_file_desc = 0; // set input fd to stdin
+        char *input_str_buffer = malloc(250);
+        memset(input_str_buffer, 0, 250);
+        int scan_items = fscanf(input_file_desc, "%s", input_str_buffer);
+
+        // TODO: handle blank lines and comments, which are lines beginning with the character `#'
+        // blank line or comment
+        if (scan_items == 0)
+        {
+            return(0);
+        } 
+        if (input_str_buffer[0] == "#")
+        {
+            return(0);
+        }
+
+        // check command
+        char *argument = strtok(input_str_buffer, " ");
+        char *argument_arr[50] = {0};
+        int i = 0;
+        while (argument != NULL)
+        {
+            argument_arr[i] = argument;
+            argument = strtok(input_str_buffer, " ");
+        }
+
         // fork into child process
         int child_status;
         pid_t child_PID = fork();
@@ -33,41 +60,24 @@ int main(int argc, char *argv[])
         {
         // an error occured
         case (-1):
+            free(input_str_buffer);
             perror("Could not implement a proper fork.\n"
                    "Shell exiting.");
             exit(1);
 
         // child process
         case (0):
-            
-            // get input from command line
-            FILE *input_file_desc = 0; // set input fd to stdin
-            char *input_str_buffer = malloc(250);
-            memset(input_str_buffer, 0, 250);
-            int scan_items = fscanf(input_file_desc, "%s", input_str_buffer);
-
-            // TODO: handle blank lines and comments, which are lines beginning with the character `#'
-            // blank line or comment
-            if (scan_items == 0)
-            {
-                return(0);
-            } 
-            if (input_str_buffer[0] == "#")
-            {
-                return(0);
-            }
-
             return(0);
+            
 
         // parent process
         default:
             // wait for child process to return
             waitpid(child_PID, &child_status, 0);
 
+            free(input_str_buffer);
             break;
         }
-
-
     }
 
     return(0);
@@ -83,3 +93,4 @@ int main(int argc, char *argv[])
 // TODO: support input and output redirection
 
 // TODO: implement custom handlers for 2 signals, SIGINT and SIGSTP
+
