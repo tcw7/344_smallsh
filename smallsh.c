959b64d31da0d63d71def0db45a206983a7c4525
Tyler Wennstrom <tylerwennstrom@gmail.com>
2022-05-04 18:49:34 -0700

    input parsing, 163665 expansion, internal function init

--- smallsh.c
+++ smallsh.c
@@ -1,12 +1,13 @@
-/* 
-* Author: Tyler Wennstrom
-* Contact: tylerwennstrom@gmail.com | wennstrt@oregonstate.edu
-* Class: Operating Systems I
-* Assignment: smallsh
-* Description: 
-*   This file contains an implementation of a custom shell for use
-*     with UNIX operating systems.
-*/
+/**
+ * * Contact: tylerwennstrom@gmail.com | wennstrt@oregonstate.edu
+ * * Date: Spring 2022
+ * * Class: Operating Systems I
+ * * Assignment: smallsh
+ * * Description: 
+ * *   This file contains an implementation of a custom shell for use
+ * *   with UNIX operating systems.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -14,14 +15,27 @@
 #include <sys/types.h>
 #include <string.h>
 
+// function prototypes here
+void PID_to_str(pid_t pid_num, char *buffer);
+
 
 // TODO: provide prompt for running commands
-int main(int argc, char *argv[])
+int main(void)
 {
+    int break_status = 0;
+    int error_status = 0;
+    pid_t current_PID = getpid();
+    char current_PID_str[50];
+    PID_to_str(current_PID, current_PID_str);
+
+    
     // endless loop until break
-    int status = 0;
-    while (!status)
+    while (!break_status)
     {
+        // var to hold background or foreground status
+        int background_status = 0;
+        int fork_status = 0;
+
         // use the colon symbol as a prompt for each command line
         printf(": ");
 
@@ -35,59 +49,135 @@ int main(int argc, char *argv[])
         // blank line or comment
         if (scan_items == 0)
         {
-            return(0);
+            continue;
         } 
-        if (input_str_buffer[0] == "#")
+        if (input_str_buffer[0] == '#')
         {
-            return(0);
+            continue;;
         }
 
-        // check command
+        // check command input
         char *argument = strtok(input_str_buffer, " ");
         char *argument_arr[50] = {0};
-        int i = 0;
+        int arg_count = 0;
         while (argument != NULL)
         {
-            argument_arr[i] = argument;
-            i++;
+            // parse argument for "$$"
+            int length = strlen(argument);
+            char *temp = malloc(250);
+            char temp_char[2] = "\0\0";
+            for (int j = 0; j < length - 1; j++)
+            {
+                if (argument[j] == '$' && 
+                    argument[j+1] == '$')
+                {
+                    strcat(temp, current_PID_str);
+                    j++;
+                }
+                else
+                {
+                    temp_char[0] = argument[j];
+                    strcat(temp, temp_char);
+                }
+            }
+            int temp_len = strlen(temp);
+            char new_arg[temp_len + 1];
+            new_arg[temp_len] = 0;
+            strcpy(new_arg, temp);
+
+            // add argument to vector/array
+            argument_arr[arg_count] = new_arg;
+            arg_count++;
             argument = strtok(input_str_buffer, " ");
+            free(temp);
+        }
+        free(input_str_buffer);
+
+        // check if background process
+        if (strcmp(argument_arr[arg_count - 1], "&") == 0)
+        {
+            background_status = WNOHANG;
         }
 
-        // fork into child process
-        int child_status;
-        pid_t child_PID = fork();
-
-        switch (child_PID)
+        // TODO: execute 3 commands: `exit' `cd' `status' via built in code
+        // check if internal process
+        if (strcmp(argument_arr[0], "exit") == 0)
         {
-        // an error occured
-        case (-1):
-            free(input_str_buffer);
-            perror("Could not implement a proper fork.\n"
-                   "Shell exiting.");
-            exit(1);
+            if ((arg_count - background_status) > 1)
+            {
+                printf("ERROR\n"
+                "`exit' usage: exit\n"
+                "`exit' takes no arguments\n");
+                continue;
+            }
+            smallsh_exit(&break_status);
+            continue;
+        }
+        else if (strcmp(argument_arr[0], "cd") == 0)
+        {
+            if ((arg_count - background_status) > 2)
+            {
+                printf("ERROR\n"
+                "`cd' usage: cd [path to directory]\n");
+                continue;
+            }
+            smallsh_cd(argument_arr[1]);
+            continue;
+        }
+        else if (strcmp(argument_arr[0], "status") == 0)
+        {
+            if ((arg_count - background_status) > 1)
+            {
+                printf("ERROR\n"
+                "`status' usage: status\n"
+                "`status' takes no arguments");
+                continue;
+            }
+            smallsh_status();
+            continue;
+        }
+        else
+        {
+            fork_status = 1;
+        }
 
-        // child process
-        case (0):
-            return(0);
-            
+        if (fork_status)
+        {
+            // fork into child process
+            int child_status;
+            pid_t child_PID = fork();
 
-        // parent process
-        default:
-            // wait for child process to return
-            waitpid(child_PID, &child_status, 0);
+            switch (child_PID)
+            {
+            // an error occured
+            case (-1):
+                perror("Could not implement a proper fork.\n"
+                    "Shell exiting.");
+                break_status = 1;
+                error_status = 1;
+                break;
 
-            free(input_str_buffer);
-            break;
+            // child process
+            case (0):
+                return(0);
+                
+
+            // parent process
+            default:
+
+                // wait for child process to return
+                waitpid(child_PID, &child_status, background_status);
+                break;
+            }
         }
     }
 
-    return(0);
+    return(error_status);
 }
 
 
 // TODO: provide expansion for the variable `$$'
 
-// TODO: execute 3 commands: `exit' `cd' `status' via built in code
 
 // TODO: execture other commands by creating new processes using a function from the `exec()' family of functions
 
@@ -95,3 +185,54 @@ int main(int argc, char *argv[])
 
 // TODO: implement custom handlers for 2 signals, SIGINT and SIGSTP
 
+/**
+ * @brief exits smallsh. takes no arguments. when this command is run,
+ * the smallsh will kill any other processes or jobs that were started
+ * before terminating itself.
+ * 
+ * @param shell_status pointer for shell status to end the process.
+ */
+void smallsh_exit(int *shell_status)
+{
+    *shell_status = 1;
+    return;
+}
+
+/**
+ * @brief changes the working directory of smallsh to path.
+ * 
+ * @param argv if path set to an empty string, then cd will change 
+ * directory to path specified in the HOME environment variable. 
+ * allowed to be either absolute or relative.
+ * 
+ */
+void smallsh_cd(char *path)
+{
+    return;
+}
+
+/**
+ * @brief prints out either the exit status or the terminating signal of 
+ * the last foreground process ran by smallsh.
+ * it will return 0 if run before any foreground process, or
+ * in the case where any of the functions `exit', `cd', or `status'
+ * have been called but no others.
+ * 
+ */
+int smallsh_status(void)
+{
+    return;
+}
+
+/**
+ * @brief converts a pid_t number to a string
+ * 
+ * @param pid_num 
+ * @param buffer holds the result string
+ */
+void PID_to_str(pid_t pid_num, char *buffer)
+{
+    int pid_int = (int) pid_num;
+    sprintf(buffer, "%d", pid_int);
+    return;
+}
